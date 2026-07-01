0ee5c5df84dfa6e99588d0065fed757fdba3500c
Danyl <danyloonopko@gmail.com>
2026-07-01 02:30:16 +0200

    added the enter button to switch the color!

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const colorSwitchButton = document.getElementById('color-switch');
 colorSwitchButton.addEventListener('click', function() {
     document.body.classList.toggle('dark');
 })
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') document.body.classList.toggle('dark');
+})
